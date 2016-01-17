8e818c92288dbe976c8d71dad8eb3efde53c8aa1
Benjamin Sloan <brsloan@gmail.com>
2016-01-17 07:38:09 +0000

    Styled index.html and revised its contents.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ require('dotenv').load();
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://heroku_vhvttgwn:b3slpi3oal2gjc451sdo0udhmq@ds047325.mongolab.com:47325/heroku_vhvttgwn');
 
+app.use('/public', express.static(process.cwd() + '/public'));
+
 routes(app);
 
 var port = process.env.PORT || 8080;
