55d2b530fbcdccb9f01ff8dfd55ac10227b31ac3
Benjamin Sloan <brsloan@gmail.com>
2016-01-21 21:47:26 +0000

    Fixed .env vars

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var routes = require('./app/routes/index.js');
 require('dotenv').load();
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://heroku_vhvttgwn:b3slpi3oal2gjc451sdo0udhmq@ds047325.mongolab.com:47325/heroku_vhvttgwn');
+mongoose.connect(process.env.MONGO_URI);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
